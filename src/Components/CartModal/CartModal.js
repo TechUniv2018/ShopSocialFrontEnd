170a2b357b51dc9dbe7b696b4961e0432dda00fc
maheshhp <hpmahesh73@gmail.com>
2018-03-03 22:03:14 +0530

    Basic layout of cart modal complete

--- src/Components/CartModal/CartModal.js
+++ src/Components/CartModal/CartModal.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import './CartModal.css';
 
 
 class CartModal extends React.Component {
   render() {
+    const cartItems = [];
+    Object.entries(this.props.cartContents)
+      .forEach(([productId, { productName, cartId, imageUrl }]) => {
+        console.log(productId, productName, cartId, imageUrl);
+        cartItems.push((<p>{productName}</p>));
+      });
     return (
-      <div />
+      <div >
+        <Modal show={this.props.cartState} onHide={this.props.hideCart}>
+          <Modal.Header>
+            <Modal.Title>Cart</Modal.Title>
+          </Modal.Header>
+          <Modal.Body >
+            {cartItems}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.props.hideCart}>Close</Button>
+            <Button onClick={() => { console.log('Performing checkout'); }}>Checkout</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
+
+CartModal.propTypes = {
+  cartState: PropTypes.bool.isRequired,
+  hideCart: PropTypes.func.isRequired,
+  cartContents: PropTypes.object.isRequired,
+};
 
 export default CartModal;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import Adminconsole from './Components/Adminconsole/Adminconsole';
-// import ProductDisplay from './Components/ProductDisplay/ProductDisplay';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+import './index.css';
 
 ReactDOM.render(
   (
