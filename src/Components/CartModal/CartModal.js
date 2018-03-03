73b6ec99bc070a8eb2fa945b20bfcbfcc420eda1
maheshhp <hpmahesh73@gmail.com>
2018-03-03 20:11:35 +0530

    Added base code for Cart Modal

---
+++ src/Components/CartModal/CartModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CartModal.css';
+
+
+class CartModal extends React.Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default CartModal;
