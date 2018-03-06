51a4d2ab991ad96d3a9b1ed3be06694f3b27bfb9
Aakash Verma <aku06_ran@yahoo.co.in>
2018-03-07 01:27:00 +0530

    Added provider to App

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Adminconsole from './Components/Adminconsole/Adminconsole';
 import ProductDisplay from './Components/ProductDisplay/ProductDisplay';
-
+import ProductCategory from './Components/ProductCategory/ProductCategory';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/admin" component={Adminconsole} />
-        <Route path="/showProduct" component={ProductDisplay} />
-
+        <Route path="/product" component={ProductDisplay} />
+        <Route path="/category/:cgory" component={ProductCategory} />
       </Switch>
     );
   }
