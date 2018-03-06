83aeb1d96d585638505bc7ff0c0748ab16c895e5
Aakash Verma <aku06_ran@yahoo.co.in>
2018-03-07 00:00:01 +0530

    ProductBlock -Functionality added

--- src/Components/ProductCategory/ProductBlock/ProductBlock.js
+++ src/Components/ProductCategory/ProductBlock/ProductBlock.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './ProductBlock.css';
 
 export default class ProductBlock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addToCartIcon: 'ion-android-cart',
+    };
+  }
+
+  addToCart =() => {
+    if (this.state.addToCartIcon === 'ion-android-cart') {
+      const productToBeAdded = {
+        productId: this.props.id,
+        cartId: this.props.cartId,
+      };
+      fetch('/api/v1/cart/addToCart', {
+        method: 'POST',
+        body: JSON.stringify(productToBeAdded),
+      }).then(response => response.json())
+        .then((responseJSON) => {
+          if (responseJSON.statusCode === 201) {
+            this.setState({
+              addToCartIcon: 'ion-checkmark-round',
+            });
+          }
+        });
+    } else if (this.state.addToCartIcon === 'ion-checkmark-round') {
+      const productToBeRemoved = {
+        productId: this.props.id,
+        cartId: this.props.cartId,
+      };
+      fetch('/api/v1/cart/removeFromCart', {
+        method: 'DELETE',
+        body: JSON.stringify(productToBeRemoved),
+      }).then(response => response.json())
+        .then((responseJSON) => {
+          if (responseJSON.statusCode === 200) {
+            this.setState({
+              addToCartIcon: 'ion-android-cart',
+            });
+          }
+        });
+    }
+  }
+
   render() {
     return (
       <figure className="ProductBox">
@@ -13,9 +57,18 @@ export default class ProductBlock extends React.Component {
           <div className="ItemPrice">
             <s>{`$${Number(this.props.price) - 5}`}</s>{`$${this.props.price}`}
           </div>
-        </figcaption><i className="ion-android-cart" />
-        <Link to={`/product/${this.props.id}`} activeClassName="AddToCart" />
+        </figcaption><i className={this.state.addToCartIcon} />
+        <Link to={this.addToCart} activeClassName="AddToCart" />
       </figure>
     );
   }
 }
+
+ProductBlock.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
+  cartId: PropTypes.string.isRequired,
+};
