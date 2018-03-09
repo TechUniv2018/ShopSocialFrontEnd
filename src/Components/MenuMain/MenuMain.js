9c61c3064efe579f6357f2517158277fbe104da7
maheshhp <hpmahesh73@gmail.com>
2018-03-09 16:28:28 +0530

    Integrated login feature and improved styling of login/registration modal

--- src/Components/MenuMain/MenuMain.js
+++ src/Components/MenuMain/MenuMain.js
@@ -1,12 +1,69 @@
 import React from 'react';
+import axios from 'axios';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import PropTypes from 'prop-types';
-
+import RegisterLoginModal from '../RegisterLoginModal/RegisterLoginModal';
 import './MenuMain.css';
 
 class MenuMain extends React.Component {
+  state={
+    isAuthenticated: false,
+    userId: '',
+    cartId: '',
+    userName: '',
+    userEmail: '',
+    showLogin: false,
+  }
+  componentDidMount() {
+    if (window.localStorage.getItem('email') !== null) {
+      this.setState({
+        userEmail: window.localStorage.getItem('email'),
+        cartId: window.localStorage.getItem('cartID'),
+        userName: window.localStorage.getItem('name'),
+        userId: window.localStorage.getItem('userID'),
+        isAuthenticated: true,
+      });
+    }
+  }
+  onLogin = (userObject) => {
+    this.setState({
+      userId: userObject.userID,
+      cartId: userObject.cartID,
+      userEmail: userObject.email,
+      userName: userObject.name,
+      isAuthenticated: true,
+      showLogin: false,
+    });
+  }
+  onLogout = () => {
+    axios.get('/user/logout').then((logoutResponse) => {
+      if (logoutResponse.data.statusCode === 200) {
+        window.localStorage.removeItem('email');
+        window.localStorage.removeItem('cartID');
+        window.localStorage.removeItem('name');
+        window.localStorage.removeItem('userID');
+        this.setState({
+          isAuthenticated: false,
+          userId: '',
+          cartId: '',
+          userName: '',
+          userEmail: '',
+          showLogin: false,
+        });
+      }
+    });
+  }
+  handleLoginModalClose = () => {
+    this.setState({
+      showLogin: false,
+    });
+  }
+  handleLoginModalOpen = () => {
+    this.setState({
+      showLogin: true,
+    });
+  }
   render() {
-    if (this.props.isAuthenticated === 'no') {
+    if (!this.state.isAuthenticated) {
       return (
         <Navbar className="NavbarMain">
           <Navbar.Header>
@@ -18,10 +75,15 @@ class MenuMain extends React.Component {
           <Navbar.Collapse>
             <Nav pullRight classname="NavbarMain">
               <NavItem eventKey={1} >
-                <div className="NavbarText">Login / Register</div>
+                <div className="NavbarText" onClick={() => { this.handleLoginModalOpen(); }}>Login / Register</div>
               </NavItem>
             </Nav>
           </Navbar.Collapse>
+          <RegisterLoginModal
+            onLogin={this.onLogin}
+            showLogin={this.state.showLogin}
+            handleClose={this.handleLoginModalClose}
+          />
         </Navbar>
       );
     }
@@ -36,7 +98,16 @@ class MenuMain extends React.Component {
         <Navbar.Collapse>
           <Nav pullRight classname="NavbarMain">
             <NavItem eventKey={1} href="#">
-              <div className="NavbarText">Logout</div>
+              <div className="NavbarText">Together</div>
+            </NavItem>
+            <NavItem eventKey={1} href="#">
+              <div className="NavbarText">Cart</div>
+            </NavItem>
+            <NavItem eventKey={1} href="#">
+              <div className="NavbarText">Hi {this.state.userName}</div>
+            </NavItem>
+            <NavItem eventKey={1} href="#">
+              <div className="NavbarText" onClick={() => { this.onLogout(); }}>Logout</div>
             </NavItem>
           </Nav>
         </Navbar.Collapse>
@@ -44,9 +115,5 @@ class MenuMain extends React.Component {
     );
   }
 }
-
-MenuMain.propTypes = {
-  isAuthenticated: PropTypes.string.isRequired,
-};
 
 export default MenuMain;
