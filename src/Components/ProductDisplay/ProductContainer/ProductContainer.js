0c499a6b400c80aaab14dc4d7d32a98c3edf8200
Aakash Verma <aku06_ran@yahoo.co.in>
2018-03-12 08:48:28 +0530

    AddToCart toggle works

--- src/Components/ProductDisplay/ProductContainer/ProductContainer.js
+++ src/Components/ProductDisplay/ProductContainer/ProductContainer.js
@@ -1,38 +1,15 @@
 import React from 'react';
-import ContentLoader from 'react-content-loader';
 import { Grid, Row, Col, Image, Button } from 'react-bootstrap';
 
 import './ProductContainer.css';
 
 const requestUrlbyId = '/api/v1/products/';
 
-const MyLoader = () => (
-  <ContentLoader
-    height={200}
-    width={400}
-    speed={2}
-    primaryColor="#100922"
-    secondaryColor="#ecebeb"
-  >
-    <rect x="226" y="60" rx="3" ry="3" width="70" height="10" />
-    <rect x="220" y="132" rx="3" ry="3" width="100" height="10" />
-    <rect x="313" y="59" rx="3" ry="3" width="10" height="10" />
-    <rect x="208" y="107" rx="3" ry="3" width="130" height="10" />
-    <rect x="206" y="84" rx="3" ry="3" width="130" height="10" />
-    <rect x="118" y="133" rx="3" ry="3" width="90" height="10" />
-    <rect x="133" y="107" rx="3" ry="3" width="60" height="10" />
-    <rect x="333" y="132" rx="3" ry="3" width="60" height="10" />
-    <rect x="351" y="106" rx="3" ry="3" width="30" height="10" />
-    <rect x="9.5" y="23.05" rx="0" ry="0" width="97" height="157" />
-    <rect x="146.5" y="42.05" rx="0" ry="0" width="0" height="0" />
-  </ContentLoader>
-);
 class ProductContainer extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      show: 'after',
       productName: 'No product to Show',
       productPrice: '',
       productDesc: '',
@@ -45,6 +22,7 @@ class ProductContainer extends React.Component {
       userId: -1,
     };
   }
+
   componentDidMount() {
     const currentUrl = (window.location.href);
     const result = currentUrl.substring(currentUrl.lastIndexOf('/') + 1);
@@ -69,6 +47,13 @@ class ProductContainer extends React.Component {
                 cartId: window.localStorage.getItem('cartID'),
                 userId: window.localStorage.getItem('userID'),
               });
+              JSON.parse(window.localStorage.getItem('cartContents')).forEach((product) => {
+                if (product.productID === this.state.productId) {
+                  this.setState({
+                    productCartStatus: 1,
+                  });
+                }
+              });
             } else {
               this.setState({
                 productName: 'No product to Show',
@@ -80,37 +65,51 @@ class ProductContainer extends React.Component {
   }
 
   addProductToCart = () => {
-    fetch('/api/v1/cart/addToCart', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        cartId: this.state.cartId,
-        productId: this.state.productId,
-        userId: this.state.userId,
-      }),
-    })
-      .then(response => response.json())
-      .then((responseJSON) => {
-        if (responseJSON.statusCode === 201) {
-          this.setState({
-            productCartStatus: 1,
-          });
-          window.localStorage.setItem('cartContents', JSON.stringify([...JSON.parse(window.localStorage.getItem('cartContents')),
-            {
-              productID: this.state.productId,
-              name: this.state.productName,
-              image: this.state.productImage,
-              price: this.state.productPrice,
-            },
-          ]));
-        }
+    if (this.state.productCartStatus === 0) {
+      fetch('/api/v1/cart/addToCart', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cartId: this.state.cartId,
+          productId: this.state.productId,
+          userId: this.state.userId,
+        }),
       })
-      .catch((error) => {
-        console.log(error);
+        .then(response => response.json())
+        .then((responseJSON) => {
+          if (responseJSON.statusCode === 201) {
+            this.setState({
+              productCartStatus: 1,
+            });
+            window.localStorage.setItem('cartContents', JSON.stringify([...JSON.parse(window.localStorage.getItem('cartContents')),
+              {
+                productID: this.state.productId,
+                name: this.state.productName,
+                image: this.state.productImage,
+                price: this.state.productPrice,
+              },
+            ]));
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      const cartContents = JSON.parse(window.localStorage.getItem('cartContents'));
+      const tempProdArr = [];
+      cartContents.forEach((product) => {
+        if (product.productID !== this.state.productId) {
+          tempProdArr.push(product);
+        }
       });
+      window.localStorage.setItem('cartContents', JSON.stringify(tempProdArr));
+      this.setState({
+        productCartStatus: 0,
+      });
+    }
   }
 
   render() {
