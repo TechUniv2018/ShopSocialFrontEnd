4f9222ab3294e37c9a68385bf861ada86747748c
maheshhp <hpmahesh73@gmail.com>
2018-03-13 20:32:54 +0530

    Modified the design of footer

--- src/Components/FooterMain/FooterMain.js
+++ src/Components/FooterMain/FooterMain.js
@@ -3,6 +3,7 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faPhone, faLocationArrow, faEnvelope, faCheck, faHeart } from '@fortawesome/fontawesome-free-solid';
 
+import './FooterMain.css';
 
 class FooterMain extends React.Component {
   render() {
@@ -12,30 +13,26 @@ class FooterMain extends React.Component {
           <Row className="show-grid">
             <Col xs={12} md={4}>
               <div>
-                <h2 style={{ 'text-align': 'center' }}> Contact Us </h2>
+                <h3 className="FooterCenterText"> Contact Us </h3>
                 <br />
                 <div>
-                  <h4> <FontAwesomeIcon icon={faPhone} /> 9878976765 </h4>
-                  <br />
-                  <h4> <FontAwesomeIcon icon={faEnvelope} /> help@shopsocial.com</h4>
-                  <br />
-                  <h4> <FontAwesomeIcon icon={faLocationArrow} /> 7th Floor, Nalapad Brigade Center, Bengaluru, Karnataka, India </h4>
+                  <h5> <FontAwesomeIcon icon={faPhone} /> 9878976765 </h5>
+                  <h5> <FontAwesomeIcon icon={faEnvelope} /> help@shopsocial.com</h5>
+                  <h5> <FontAwesomeIcon icon={faLocationArrow} /> 7th Floor, Nalapad Brigade Center, Bengaluru, Karnataka, India </h5>
                 </div>
               </div>
             </Col>
             <Col xs={12} md={5} mdOffset={3}>
               <div>
-                <h2 style={{ 'text-align': 'center' }}> Terms and Conditions </h2>
+                <h3 className="FooterCenterText"> Terms and Conditions </h3>
                 <br />
-                <div style={{ 'text-align': 'justify' }}>
+                <div className="FooterJustifyText">
                   <h6> <FontAwesomeIcon icon={faCheck} /> Use of the Website is available only to persons who can form legally
                     binding contracts under Indian Contract Act, 1872.
                   </h6>
-                  <br />
                   <h6> <FontAwesomeIcon icon={faCheck} /> As a minor if you wish to use or transact on website, such use or transaction may
                     be made by your legal guardian or parents on the Website.
                   </h6>
-                  <br />
                   <h6> <FontAwesomeIcon icon={faCheck} /> If You use the Website, You shall be responsible for maintaining the confidentiality of your Display Name and Password and You shall be responsible for all activities that occur
                      under your Display Name and Password.
                   </h6>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Grid, Row, Col } from 'react-bootstrap';
-import FooterMain from './FooterMain/FooterMain';
+import FooterMain from '../FooterMain/FooterMain';
 import './Home.css';
 
 
